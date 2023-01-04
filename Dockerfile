16c8d7abd89e1a1f641a35df41e8892d5ceda0fc
Andrey Dyatlov <fantazey@gmail.com>
2023-01-05 02:16:52 +0300

    fix dockerfile fix deploy sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM python:3.9.13-buster
+FROM python:3.9.13-slim-buster
 WORKDIR /usr/src/app
 COPY req.txt ./
+RUN apt-get install python3-lxml
 RUN pip install --no-cache-dir -r req.txt
 COPY . .
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 ssh pi@pi << HERE
 cd /home/pi/Projects/home-api
 git pull origin master
+docker build -t home-api .
 docker stop test-django
 docker rm test-django
-docker build -t home-api .
 docker run -d --name=test-django -p 8001:8000 --add-host=pi:192.168.0.133 home-api
 HERE
