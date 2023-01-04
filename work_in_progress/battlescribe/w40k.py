b58dcda9021532264be8cc347fde7886b443d806
Andrey Dyatlov <fantazey@gmail.com>
2023-01-04 22:35:25 +0300

    add bs models fill bs models add time field to model progress update req.txt

---
+++ work_in_progress/battlescribe/w40k.py
@@ -0,0 +1,45 @@
+from os import walk
+from os import path
+
+from bs4 import BeautifulSoup
+from work_in_progress.models import BSUnit, BSCategory
+
+SOURCE_PATH = path.join(path.dirname(path.abspath(__file__)), 'fixtures')
+
+
+def main():
+    """
+    Читаем фикстуры скачанные с https://github.com/BSData/wh40k
+    сохраняем пока только часть данных себе в базу
+    """
+    for root, dirs, files in walk(SOURCE_PATH):
+        for file in files:
+            print('read:', file)
+            read_fixture_file(path.join(root, file))
+
+
+def read_fixture_file(source):
+    """
+    Читаем
+    :param source:
+    :return:
+    """
+    with open(source, 'r') as f:
+        s = f.readlines()
+        soup = BeautifulSoup("".join(s), 'xml')
+    is_common = soup.find_all('gameSystem')
+    if len(is_common) > 0:
+        print('40k base rules gamesystem file')
+        return
+
+    units_entries = soup.find_all('selectionEntry', {'type': 'unit'})
+    category_name = soup.find('catalogue').attrs['name']
+    print("read catalog for:", category_name)
+    category, created = BSCategory.objects.get_or_create(name=category_name, source=source)
+    print("category: ", category, " created:", created)
+    for entry in units_entries:
+        name = entry.attrs['name']
+        print("unit entry:", name)
+        if not BSUnit.objects.filter(name=name, bs_category=category).exists():
+            unit = BSUnit(name=name, bs_category=category)
+            unit.save()
--- work_in_progress/models.py
+++ work_in_progress/models.py
@@ -4,12 +4,40 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class BSCategory(models.Model):
+    """
+    Категория прочитананя из каталога данных BattleScribe
+    """
+    name = models.CharField(verbose_name="Название", max_length=500)
+    source = models.CharField(verbose_name="Источник", max_length=500)
+
+    def __str__(self):
+        return "%s(%s)" % (self.name, self.source)
+
+    def __unicode__(self):
+        return "%s(%s)" % (self.name, self.source)
+
+
+class BSUnit(models.Model):
+    """
+    Юнит прочитанный из каталога BattleScribe
+    """
+    name = models.CharField(verbose_name="Название", max_length=500)
+    bs_category = models.ForeignKey(BSCategory, on_delete=models.RESTRICT, verbose_name="Тип", null=True)
+
+
 class Model(models.Model):
+    """
+    Модель над которой идет работа. Может состоять из одной или нескольких миниатюр. Считаем что модель это коробка
+    """
     class Status(models.TextChoices):
+        """
+        Статусы через которые проходит модель в процессе работы над ней
+        """
         WISHED = 'wished', 'Лежит в магазине'
         IN_INVENTORY = 'in_inventory', 'Лежит в шкафу'
         ASSEMBLING = 'assembling', 'Собирается'
-        ASSEMBLED = 'assembled', 'Собирано'
+        ASSEMBLED = 'assembled', 'Собрано'
         PRIMING = 'priming', 'Грунтуется'
         PRIMED = 'primed', 'Загрунтовано'
         BATTLE_READY_PAINTING = 'battle_ready_painting', 'Крашу в базу'
@@ -22,6 +50,7 @@ class Model(models.Model):
         DONE = 'done', 'Закончено'
 
     name = models.CharField(verbose_name="Название модели", max_length=500)
+    battlescribe_unit = models.ForeignKey(BSUnit, verbose_name="Из каталога BS", on_delete=models.RESTRICT, null=True)
     status = models.CharField(verbose_name="Статус", max_length=200, choices=Status.choices, default=Status.WISHED)
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.RESTRICT)
     created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True, null=False, blank=False)
@@ -80,8 +109,18 @@ class Model(models.Model):
 
 
 class ModelProgress(models.Model):
+    """
+        Запись о работе над моделью
+    """
     title = models.CharField(verbose_name="Проводимые работы", max_length=500)
     description = models.TextField(verbose_name="Подробности выполнененной работы")
     datetime = models.DateTimeField(verbose_name="Дата записи")
+    time = models.FloatField(verbose_name="Затраченое время в часах", default=0.0)
     model = models.ForeignKey(Model, on_delete=models.RESTRICT, verbose_name="Прогресс")
+
+    def __str__(self):
+        return "%s - %s - %s - %s" % (self.model.name, self.title, self.time, self.datetime)
+
+    def __unicode__(self):
+        return "%s - %s - %s - %s" % (self.model.name, self.title, self.time, self.datetime)
 
