68f61d1cbdbaa51de98eed93e3de81d7424382c8
Andrey Dyatlov <andrew@MacBook-Air-Andrey.local>
2023-01-04 14:25:33 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+ssh pi@pi << HERE
 cd /home/pi/Projects/home-api
 git pull origin master
 docker stop test-django
 docker rm test-django
 docker build -t home-api .
 docker run -d --name=test-django -p 8001:8000 --add-host=pi:192.168.0.133 home-api
+HERE
