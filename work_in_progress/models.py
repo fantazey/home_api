efff95b774f2badb125b8eef4fede3cf75cf62aa
Andrey Dyatlov <fantazey@gmail.com>
2023-01-05 18:29:21 +0300

    fix header menu errors, add spent hours to table

--- work_in_progress/models.py
+++ work_in_progress/models.py
@@ -130,6 +130,9 @@ class Model(models.Model):
     def get_days_since_buy(self):
         return (datetime.datetime.now().date() - self.buy_date).days
 
+    def get_hours_spent(self):
+        return self.modelprogress_set.aggregate(models.Sum('time'))['time__sum']
+
 
 class ModelProgress(models.Model):
     """
