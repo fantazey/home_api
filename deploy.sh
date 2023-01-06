62f4ba49e42852515631fd10e780c5b00721dfe8
Andrey Dyatlov <fantazey@gmail.com>
2023-01-06 15:40:27 +0300

    fix req fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git pull origin master
 docker build -t home-api .
 docker stop test-django
 docker rm test-django
-docker run -d --name=test-django -p 8001:8000 --add-host=pi:192.168.0.133 home-api
+docker run -d --name=test-django -p 8001:8000 -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api
 HERE
