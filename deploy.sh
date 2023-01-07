8c8d8744c57d35671f9c1034856a24856777b18a
Andrey Dyatlov <andrew@MacBook-Air-Andrey.local>
2023-01-07 21:39:43 +0300

    update deploy.sh add run telebot

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@ docker build -t home-api .
 docker stop test-django
 docker rm test-django
 docker run -d --name=test-django -p 8001:8000 -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api
+docker build -f Dockerfile_telebot -t home-api-telebot .
+docker stop test-home-api-telebot
+docker rm test-home-api-telebot
+docker run -d --name=test-home-api-telebot -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api-telebot
 HERE
