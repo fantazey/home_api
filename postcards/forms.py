c98c288775ffa44e4f5199d360bf4ddb1db477a4
Oksana <oksana.con@mail.ru>
2024-02-29 14:52:20 +0300

    добавили комментарии к моделям

--- postcards/forms.py
+++ postcards/forms.py
@@ -1,28 +1,31 @@
-from django.forms import ModelForm,ImageField, CharField, IntegerField, ChoiceField, Form
+from django.forms import ModelForm, ImageField, CharField, IntegerField, ChoiceField, Form, DateField
 
 from .models import Postcard
 
 
 def build_country_choices():
     c = ['Россия', 'Германия', 'Нидерланды']
-    return [(x,x) for x in c]
+    return [(x, x) for x in c]
 
 
 class PostcardForm(ModelForm):
-    """
-
-    """
     image = ImageField(label='фотка')
     sender = CharField(max_length="50", label="отправитель")
     country = ChoiceField(label='страна', choices=build_country_choices)
     travel_time = IntegerField(label="время в пути")
+    date_receiving = DateField(label='дата получения')
 
     class Meta:
         model = Postcard
-        fields = ('image', 'sender', 'country', 'travel_time')
+        fields = ('image', 'sender', 'country', 'travel_time', 'date_receiving')
+
 
 class LibraryAddForm(Form):
-    """
-
-    """
     image = ImageField(label='фотка')
+
+
+class AddressAddForm(ModelForm):
+    image = ImageField(label='желаемая открытка')
+    name = CharField(max_length="70", label='имя получателя')
+    city = CharField(max_length="30", label='город получателя')
+    address = CharField(max_length="70", label='адерс с индексом')
--- postcards/models.py
+++ postcards/models.py
@@ -1,24 +1,28 @@
+import datetime
+
 from django.db import models
 
 
 class Postcard(models.Model):
-    """
+    '''
+    определили модель карточки открытки. Содержит в себе фотографию открытки, имя и страну отправителя, время в пути
+    '''
 
-    """
     image = models.ImageField(upload_to='postcard', verbose_name='лицевая сторона')
     sender = models.CharField(max_length=50, verbose_name='имя отправителя')
     country = models.CharField(max_length=50, verbose_name='страна отправления')
     travel_time = models.IntegerField(verbose_name='дней в пути')
+    date_receiving = models.DateField(verbose_name='дата получения открытки', default=datetime.date.today())
+
 
     class Meta:
         verbose_name = 'Полученная открытка'
         verbose_name_plural = 'Полученные открытки'
 
+    '''определили модели карточки открытки из бибилиотеки открыток к отправке'''
+
 
 class Library(models.Model):
-    """
-
-    """
     image = models.ImageField(upload_to='library', verbose_name='картинка')
 
     class Meta:
