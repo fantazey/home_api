9db98421c184f51667de0435b34d1e197680ac2b
Andrey Dyatlov <fantazey@gmail.com>
2024-03-13 22:25:26 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ ssh pi@pi << HERE
 cd /home/pi/Projects/home-api
 git pull origin master
 docker build -t home-api .
-docker stop test-django
-docker rm test-django
-docker run -d --name=test-django -p 8001:8000 -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api
+docker stop home-api-web
+docker rm home-api-web
+docker run -d --name=home-api-web --restart=unless-stopped -p 8001:8000 -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api
 docker build -f Dockerfile_telebot -t home-api-telebot .
-docker stop test-home-api-telebot
-docker rm test-home-api-telebot
-docker run -d --name=test-home-api-telebot -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api-telebot
+docker stop home-api-telebot
+docker rm home-api-telebot
+docker run -d --name=home-api-telebot --restart=unless-stopped -v /tmp/homeapi:/tmp/homeapi -v /home/pi/Projects/home-api/media:/usr/src/app/media --add-host=pi:192.168.0.133 home-api-telebot
 HERE
