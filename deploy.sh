22ff34a0769e703da33c8cec909abaae4e806e68
Andrey Dyatlov <fantazey@gmail.com>
2024-03-22 19:05:02 +0300

    update gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 # build base image for rpi3 model b
 #docker buildx build -f Dockerfile_base --load --platform linux/arm/v7 -t fantazey/home-api-base:slim .
+#web
 # build web image for rpi3 model b
-#docker buildx build -f Dockerfile_web --load --platform linux/arm/v7 -t home-api .
-# build telebot image for rpi3 model b
-#docker buildx build -f Dockerfile_telebot --load --platform linux/arm/v7 -t home-api-telebot .
+docker buildx build -f Dockerfile_web --load --platform linux/arm/v7 -t home-api:slim .
 docker save -o /tmp/docker-image-home-api.tar home-api:slim
+gzip /tmp/docker-image-home-api.tar
+scp /tmp/docker-image-home-api.tar.gz pi@pi:/tmp/docker-image-home-api.tar.gz
+rm /tmp/docker-image-home-api.tar.gz
+# telebot
+# build telebot image for rpi3 model b
+docker buildx build -f Dockerfile_telebot --load --platform linux/arm/v7 -t home-api-telebot .
 docker save -o /tmp/docker-image-home-api-telebot.tar home-api-telebot:slim
-scp /tmp/docker-image-home-api.tar pi@pi:/tmp/docker-image-home-api.tar
-scp /tmp/docker-image-home-api-telebot.tar pi@pi:/tmp/docker-image-home-api-telebot.tar
-rm /tmp/docker-image-home-api.tar
-rm /tmp/docker-image-home-api-telebot.tar
+gzip /tmp/docker-image-home-api-telebot.tar
+scp /tmp/docker-image-home-api-telebot.tar.gz pi@pi:/tmp/docker-image-home-api-telebot.tar.gz
+rm /tmp/docker-image-home-api-telebot.tar.gz
+
 ssh pi@pi << HERE
-cd /home/pi/Projects/home-api
+cd /tmp
+gunzip /tmp/docker-image-home-api.tar.gz
 docker load -i /tmp/docker-image-home-api.tar
 rm /tmp/docker-image-home-api.tar
+
+gunzip /tmp/docker-image-home-api-telebot.tar.gz
 docker load -i /tmp/docker-image-home-api-telebot.tar
 rm /tmp/docker-image-home-api-telebot.tar
+
+cd /home/pi/Projects/home-api
 docker stop home-api-web
 docker rm home-api-web
 docker run -d --name=home-api-web \
